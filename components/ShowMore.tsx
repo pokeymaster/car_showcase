cea0fdb01930bdc15ceea3dd6821a5b05b1aaa26
pokeymaster <cyd1995@hotmail.com>
2023-06-10 23:29:36 +1000

    init

---
+++ components/ShowMore.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { ShowMoreProps } from "@/types";
+import { CustomButton } from "@/components";
+
+const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    // Calculate the new limit based on the page number and navigation type
+    const newLimit = (pageNumber + 1) * 10;
+
+    // Update the "limit" search parameter in the URL with the new value
+    setLimit(newLimit);
+  };
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          title="Show More"
+          btnType="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ShowMore;
